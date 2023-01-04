fb52457bac72a12ba904d92111f3b2ac8609c6c5
Raymond Zou <raymond.1zou@gmail.com>
2023-01-04 08:36:16 -0800

    add sidebar profile to main feed

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Homepage from './Homepage';
 import styles from './App.module.css';
 
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, signInWithPopup, GoogleAuthProvider, updateProfile } from 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 const firebaseApp = initializeApp({
@@ -37,32 +37,65 @@ function App() {
 
   return (
     <div>
-      {user ? <MainFeed/> : <Homepage signInMethod={signInWithGoogle} />}
+      {user ? <MainFeed profilePicture={user.photoURL} /> : <Homepage signInMethod={signInWithGoogle} />}
     </div>
   )
 }
 
 function MainFeed() {
+
+  const { photoURL, displayName } = auth.currentUser;
+
+  console.log(auth.currentUser.photoURL);
   return (
     <>
-      <div className={styles["nav-bar"]}>
-        <div className={styles["search-bar-container"]}>
-          <div className={styles["logo"]}>Out</div>
-          <input className={styles["search-bar"]} placeholder="Search"></input>
+      <div className={styles["main-container"]}>
+        <div className={styles["nav-bar"]}>
+          <div className={styles["search-bar-container"]}>
+            <div className={styles["logo"]}>Out</div>
+            <input className={styles["search-bar"]} placeholder="Search"></input>
+          </div>
+          <div className={styles["nav-button-container"]}>
+            <div className={styles["nav-button"]}><HomeIcon />Home</div>
+            <div className={styles["nav-button"]}><GroupIcon />My Network</div>
+            <div className={styles["nav-button"]}><BusinessCenterIcon />Jobs</div>
+            <div className={styles["nav-button"]}><TextsmsIcon />Messaging</div>
+            <div className={styles["nav-button"]}><NotificationsRoundedIcon />Notifications</div>
+            <div className={styles["nav-button"]}><img alt="profile_pic" src={photoURL} />Me</div>
+            <div className={styles["nav-button"]}><AppsIcon />Work</div>
+            <div className={styles["nav-button"]}><AutoStoriesIcon />Learning</div>
+          </div>
         </div>
-        <div className={styles["nav-button-container"]}>
-          <div className={styles["nav-button"]}><HomeIcon />Home</div>
-          <div className={styles["nav-button"]}><GroupIcon />My Network</div>
-          <div className={styles["nav-button"]}><BusinessCenterIcon />Jobs</div>
-          <div className={styles["nav-button"]}><TextsmsIcon />Messaging</div>
-          <div className={styles["nav-button"]}><NotificationsRoundedIcon />Notifications</div>
-          <div className={styles["nav-button"]}><img alt="profile_pic" src={auth.currentUser.photoURL}/>Me</div>
-          <div className={styles["nav-button"]}><AppsIcon />Work</div>
-          <div className={styles["nav-button"]}><AutoStoriesIcon />Learning</div>
+        <div>Welcome back, {auth.currentUser.displayName}!</div>
+        <button onClick={signOut}>Sign Out</button>
+        <div className={styles["scaffold-container"]}>
+          <div className={styles["scaffold-sidebar"]}>
+            <div className={styles["wrapper"]}>
+              <div className={styles["sidebar-background-img"]}>
+                <img src={
+                  photoURL ||
+                  "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+                } alt="profile_pic" />
+              </div>
+            </div>
+            <div className={styles["sidebar-text"]}>
+              <div className={styles["sidebar-header"]}>{displayName}</div>
+              <div className={styles["button-container"]}>
+                <div className={styles["sidebar-button"]}>
+                  <div>Connections</div>
+                  <div>42</div>
+                </div>
+                <div className={styles["sidebar-button"]}>
+                  <div>Who's viewed your profile</div>
+                  <div className={styles["profile-view-count"]}>502</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles["scaffold-main"]}></div>
+          <div className={styles["scaffold-aside"]}></div>
         </div>
       </div>
-      <div>Welcome back, {auth.currentUser.displayName}!</div>
-      <button onClick={signOut}>Sign Out</button>
     </>
   )
 }
