bdaa6bf330e8cf868845809ab194f33db626273d
Raymond Zou <raymond.1zou@gmail.com>
2023-01-10 12:13:34 -0800

    style file upload button

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,90 +1,114 @@
-import { useState } from 'react';
-import styles from '../styles/Overlay.module.css';
-import { UserContext, storage } from '../App';
-import { ref, getDownloadURL , uploadBytes } from 'firebase/storage';
-import { useContext } from 'react';
-import { v4 } from 'uuid';
+import { useState } from "react";
+import ImageIcon from "@mui/icons-material/ImageRounded";
+import styles from "../styles/Overlay.module.css";
+import { UserContext, storage } from "../App";
+import { ref, getDownloadURL, uploadBytes } from "firebase/storage";
+import { useContext } from "react";
+import { v4 } from "uuid";
 
 export default function PostInputContainer(props) {
+  // TODO: replace UserContext with import statments
+  // console.log(auth);
 
-    // TODO: replace UserContext with import statments
-    // console.log(auth);
+  const { displayName, photoURL } = useContext(UserContext);
+  const { setOverlay, isOpen, uploadPost } = props;
 
-    const { displayName, photoURL } = useContext(UserContext);
-    const { setOverlay, isOpen, uploadPost } = props;
+  const handleOutsideClick = () => setOverlay(false);
 
-    const handleOutsideClick = () => setOverlay(false);
-
-    return isOpen ? (<div
-        onClick={handleOutsideClick}
-        className={styles.overlay}>
-        <PostInput
-            displayName={displayName}
-            photoURL={photoURL}
-            setOverlay={setOverlay}
-            uploadPost={uploadPost} />
-    </div>)
-        : null
+  return isOpen ? (
+    <div onClick={handleOutsideClick} className={styles.overlay}>
+      <PostInput
+        displayName={displayName}
+        photoURL={photoURL}
+        setOverlay={setOverlay}
+        uploadPost={uploadPost}
+      />
+    </div>
+  ) : null;
 }
 
 function PostInput(props) {
+  const { photoURL: profileImgURL, displayName, uid } = useContext(UserContext);
+  const { setOverlay, uploadPost } = props;
+  const [postText, setPostText] = useState("");
+  const [postImg, setPostImg] = useState();
 
-    const { photoURL: profileImgURL, displayName, uid } = useContext(UserContext)
-    const { setOverlay, uploadPost } = props;
-    const [postText, setPostText] = useState('');
-    const [postImg, setPostImg] = useState();
+  const handleInsideClick = (e) => e.stopPropagation();
+  const updatePostText = (e) => setPostText(e.target.value);
 
+  console.log(postImg);
 
-    const handleInsideClick = e => e.stopPropagation();
-    const updatePostText = e => setPostText(e.target.value);
+  async function postToFirebase(e) {
+    e.preventDefault();
+    const photoURL = await uploadImg(postImg);
+    uploadPost(displayName, profileImgURL, postText, uid, photoURL);
+    setPostText("");
+    setOverlay(false);
+  }
 
-    async function postToFirebase(e) {
-        e.preventDefault();
-        const photoURL = await uploadImg(postImg);
-        uploadPost(displayName, profileImgURL, postText, uid, photoURL);
-        setPostText('');  
-        setOverlay(false);
+  async function uploadImg(img) {
+    if (img) {
+      const imgRef = ref(storage, `images/${uid}/${img.name + v4()}`);
+      await uploadBytes(imgRef, postImg);
+      return await getDownloadURL(imgRef);
+    } else {
+      return null;
     }
+  }
 
-    async function uploadImg(img) {
-        if (img) {
-            const imgRef = ref(storage, `images/${uid}/${img.name + v4()}`);
-            await uploadBytes(imgRef, postImg);
-            return await getDownloadURL(imgRef);
-        } else {
-            return null;
-        }
-    }
-
-    return <div onClick={handleInsideClick}
-        className={`${styles.createPost} ${styles.outline}`}>
-        <form>
-            <div className={styles.header}>
-                <div>Create a post</div>
-                <button className={styles.closeButton} onClick={() => setOverlay(false)}>X</button>
+  return (
+    <div
+      onClick={handleInsideClick}
+      className={`${styles.createPost} ${styles.outline}`}
+    >
+      <form>
+        <div className={styles.header}>
+          <div>Create a post</div>
+          <button
+            className={styles.closeButton}
+            onClick={() => setOverlay(false)}
+          >
+            X
+          </button>
+        </div>
+        <div className={styles.profilePicContainer}>
+          <img src={profileImgURL} alt="" />
+          <div>{displayName}</div>
+        </div>
+        <div className={styles.formContent}>
+          <textarea
+            name=""
+            id=""
+            cols="55"
+            rows="10"
+            placeholder="What do you want to talk about?"
+            onChange={updatePostText}
+            value={postText}
+          ></textarea>
+          <div className={styles.postButtonContainer}>
+            <div className={styles.fileUploadContainer}>
+              <label className={styles.fileUploadButton} htmlFor="file-upload">
+                <ImageIcon />
+                <input
+                  id="file-upload"
+                  type="file"
+                  onChange={(event) => {
+                    setPostImg(event.target.files[0]);
+                  }}
+                />
+              </label>
+              {postImg ? postImg.name.substring(0, 9) + "..." : null}
             </div>
-            <div className={styles.profilePicContainer}>
-                <img src={profileImgURL} alt="" />
-                <div>{displayName}</div>
-            </div>
-            <div className={styles.formContent}>
-                <textarea
-                    name=""
-                    id="" cols="55"
-                    rows="10"
-                    placeholder="What do you want to talk about?"
-                    onChange={updatePostText}
-                    value={postText}>
-                </textarea>
-                <input type="file" onChange={(event) => { setPostImg(event.target.files[0]) }} />
-                <div className={styles.submitButtonContainer}>
-                    <button
-                        disabled={postText === ''}
-                        className={styles.submitButton}
-                        onClick={postToFirebase}>Post
-                    </button>
-                </div>
-            </div>
-        </form>
+            <button
+              disabled={postText === ""}
+              className={styles.submitButton}
+              onClick={postToFirebase}
+            >
+              Post
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
+  );
 }
