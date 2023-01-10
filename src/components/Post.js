2ae45a674a500af87614370c2eb711531fd1da0c
Raymond Zou <raymond.1zou@gmail.com>
2023-01-09 22:49:02 -0800

    add post edit button placeholder

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,15 +1,19 @@
 import styles from '../styles/Post.module.css';
+import ThreeDots from '@mui/icons-material/MoreHoriz';
 
 export default function Post(props) {
 
     const { name, postText, photoURL, postImgURL } = props.post;
 
     return <div className={`${styles.outline} ${styles.postContainer}`}>
-        <div className={styles.posterInfoContainer}>
-            <img className={styles.profileImg} src={photoURL} alt="" />
-            <div className={styles.postNameContainer}>
-                {name}
+        <div className={styles.postHeader}>
+            <div className={styles.posterInfoContainer}>
+                <img className={styles.profileImg} src={photoURL} alt="" />
+                <div className={styles.postNameContainer}>
+                    {name}
+                </div>
             </div>
+            <div className={styles.horizontalButton}>            <ThreeDots /></div>
         </div>
         <div className={styles.postTextContainer}>
             {postText}
