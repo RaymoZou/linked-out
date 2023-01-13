f0c74760ab25fc2bead0e737eb511d0bf1feb3ac
Raymond Zou <raymond.1zou@gmail.com>
2023-01-13 09:59:02 -0800

    update sidebar styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,28 +4,33 @@ import styles from '../styles/Sidebar.module.css';
 
 export default function Sidebar() {
 
-    const {displayName, photoURL } = useContext(UserContext)
+    const { displayName, photoURL } = useContext(UserContext)
 
     return (
         <div className={`${styles.scaffoldSidebar} ${styles.outline}`}>
-            <div className={styles.wrapper}>
+            {/* <div className={styles.wrapper}>
                 <div className={styles.sidebarBackgroundImg}>
                     <img src={
                         photoURL ||
                         'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
                     } alt='profile_pic' />
                 </div>
+            </div> */}
+            <div className={styles.profilePictureContainer}>
+                <img className={styles.profilePicture} src={photoURL} alt="" />
             </div>
             <div className={styles.sidebarText}>
-                <div className={styles.sidebarHeader}>{displayName}</div>
+                <div className={styles.sidebarHeader}>
+                    <div>{displayName}</div>
+                </div>
                 <div className={styles.buttonContainer}>
                     <div className={styles.sidebarButton}>
                         <div>Connections</div>
-                        <div>42</div>
+                        {/* <div>42</div> */}
                     </div>
                     <div className={styles.sidebarButton}>
                         <div>Who's viewed your profile</div>
-                        <div className={styles.profileViewCount}>502</div>
+                        {/* <div className={styles.profileViewCount}>502</div> */}
                     </div>
                 </div>
             </div>
