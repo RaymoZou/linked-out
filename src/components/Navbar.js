57ddb7757df4301db6845290b7647e67eebcbc88
Raymond Zou <raymond.1zou@gmail.com>
2023-01-25 12:17:02 -0800

    add menu close on outside click

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,48 +1,91 @@
-import HomeIcon from '@mui/icons-material/Home';
-import PeopleIcon from '@mui/icons-material/Group';
-import SuitcaseIcon from '@mui/icons-material/BusinessCenter';
-import ChatBubbleIcon from '@mui/icons-material/Textsms';
-import BellIcon from '@mui/icons-material/Notifications';
-import AutoStoriesIcon from '@mui/icons-material/AutoStories';
-import AppsIcon from '@mui/icons-material/Apps';
-import { Menu, MenuItem } from '@mui/material';
+import HomeIcon from "@mui/icons-material/Home";
+import PeopleIcon from "@mui/icons-material/Group";
+import SuitcaseIcon from "@mui/icons-material/BusinessCenter";
+import ChatBubbleIcon from "@mui/icons-material/Textsms";
+import BellIcon from "@mui/icons-material/Notifications";
+import AutoStoriesIcon from "@mui/icons-material/AutoStories";
+import AppsIcon from "@mui/icons-material/Apps";
+import DropDownIcon from "@mui/icons-material/ArrowDropDown";
+import { Menu, MenuItem } from "@mui/material";
 
-import styles from '../styles/Navbar.module.css';
-import { UserContext } from '../App';
-import { useState, useContext } from 'react';
+import styles from "../styles/Navbar.module.css";
+import DropDownMenu from "./ui/DropDownMenu";
+import { UserContext } from "../App";
+import { useState, useContext } from "react";
 
 export default function Navbar(props) {
-
-  const [anchor, setAnchor] = useState(null);
   const user = useContext(UserContext);
+  const [isDropDown, setDropDown] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [isOpen, setOpen] = useState(false);
 
-  const openPopupMenu = (e) => { setAnchor(e.currentTarget) };
+  function handleClick(e) {
+    if (anchorEl) return;
+    setAnchorEl(e.currentTarget);
+  }
+
+  const handleClickAway = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <div className={styles.navBar}>
       <div className={styles.wrapper}>
         <div className={styles.searchBarContainer}>
           <div className={styles.logo}>out</div>
-          <input className={styles.searchBar} placeholder='Search'></input>
+          <input className={styles.searchBar} placeholder="Search"></input>
         </div>
         <div className={styles.navButtonContainer}>
-          <div className={styles.navButton}><HomeIcon />Home</div>
-          <div className={styles.navButton}><PeopleIcon />My Network</div>
-          <div className={styles.navButton}><SuitcaseIcon />Jobs</div>
-          <div className={styles.navButton}><ChatBubbleIcon />Messaging</div>
-          <div className={styles.navButton}><BellIcon />Notifications</div>
-          <button onClick={openPopupMenu} className={styles.navProfile}>
-            <img alt='profile_pic' src={user.photoURL} />
-            <div className={styles.navButtonText}>
-              <Menu open={Boolean(anchor)} anchorEl={anchor}>
-                <MenuItem>Hello</MenuItem>
-              </Menu>
-            </div>
+          <div className={styles.navButton}>
+            <HomeIcon />
+            Home
+          </div>
+          <div className={styles.navButton}>
+            <PeopleIcon />
+            My Network
+          </div>
+          <div className={styles.navButton}>
+            <SuitcaseIcon />
+            Jobs
+          </div>
+          <div className={styles.navButton}>
+            <ChatBubbleIcon />
+            Messaging
+          </div>
+          <div className={styles.navButton}>
+            <BellIcon />
+            Notifications
+          </div>
+          <button onClick={handleClick} className={styles.navProfile}>
+            <img alt="profile_pic" src={user.photoURL} />
+            <div className={styles.navButtonText}></div>
           </button>
-          <div className={styles.navButton}><AppsIcon />Work</div>
-          <div className={styles.navButton}><AutoStoriesIcon />Learning</div>
+          <div className={styles.navButton}>
+            <AppsIcon />
+            Work
+          </div>
+          <div className={styles.navButton}>
+            <AutoStoriesIcon />
+            Learning
+          </div>
         </div>
       </div>
+      <Menu
+        open={Boolean(anchorEl)}
+        anchorEl={anchorEl}
+        disableAutoFocus={true}
+        onClose={handleClickAway}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem onClick={props.signOut}>Sign Out</MenuItem>
+      </Menu>
     </div>
-  )
+  );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,66 +1,78 @@
-import styles from '../styles/Post.module.css';
-import ThreeDots from '@mui/icons-material/MoreHoriz';
-import DropDownMenu from './ui/DropDownMenu';
-import { useEffect, useState, useRef, useContext } from 'react';
-import { db, UserContext, storage } from '../App.js';
-import { deleteDoc, doc } from 'firebase/firestore';
-import { ref, deleteObject } from 'firebase/storage';
+import styles from "../styles/Post.module.css";
+import ThreeDots from "@mui/icons-material/MoreHoriz";
+import DropDownMenu from "./ui/DropDownMenu";
+import { useEffect, useState, useRef, useContext } from "react";
+import { db, UserContext, storage } from "../App.js";
+import { deleteDoc, doc } from "firebase/firestore";
+import { ref, deleteObject } from "firebase/storage";
+import { Menu, MenuItem } from "@mui/material";
 
 export default function Post(props) {
+  const currentUser = useContext(UserContext);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const btnRef = useRef();
+  const { name, postText, photoURL, postImgURL, uid, imgName, createdAt } =
+    props.post;
+  const { postId } = props;
 
-    const currentUser = useContext(UserContext);
-    const [isDropDown, setDropDown] = useState(false);
-    const btnRef = useRef();
-    const { name, postText, photoURL, postImgURL, uid, imgName, createdAt } = props.post;
-    const { postId } = props;
+  function handleClick(e) {
+    if (anchorEl) return;
+    setAnchorEl(e.currentTarget);
+  }
 
-    useEffect(() => {
+  const handleClickAway = () => {
+    setAnchorEl(null);
+  };
 
-        function closeDropDown(e) {
-            if (e.path[0] !== btnRef.current) setDropDown(false);
-        }
-        document.body.addEventListener('click', closeDropDown);
-        return () => document.body.removeEventListener('click', closeDropDown);
-    })
-
-    function renderDropDown(e) {
-        e.stopPropagation();
-        setDropDown(true);
+  function deletePost() {
+    if (imgName) {
+      const imgRef = ref(storage, `images/${currentUser.uid}/${imgName}`);
+      deleteObject(imgRef);
     }
-
-    function deletePost() {
-        if (imgName) {
-            const imgRef = ref(storage, `images/${currentUser.uid}/${imgName}`);
-            deleteObject(imgRef);
-        }
-        if (currentUser.uid === uid) {
-            const docRef = doc(db, "posts", postId);
-            deleteDoc(docRef);
-        }
+    if (currentUser.uid === uid) {
+      const docRef = doc(db, "posts", postId);
+      deleteDoc(docRef);
     }
+  }
 
-    return <div className={`${styles.outline} ${styles.container}`}>
-        <div className={styles.header}>
-            <div className={styles.posterInfo}>
-                <img className={styles.profileImg} src={photoURL} alt="" />
-                <div className={styles.nameContainer}>
-                    {name}
-                </div>
-            </div>
-            {currentUser.uid === uid ?
-                <div onClick={renderDropDown} className={styles.horizontalButton}>
-                    <ThreeDots />
-                    <DropDownMenu isOpen={isDropDown}>
+  return (
+    <div className={`${styles.outline} ${styles.container}`}>
+      <div className={styles.header}>
+        <div className={styles.posterInfo}>
+          <img className={styles.profileImg} src={photoURL} alt="" />
+          <div className={styles.nameContainer}>{name}</div>
+        </div>
+        {currentUser.uid === uid ? (
+          <div onClick={handleClick} className={styles.horizontalButton}>
+            <ThreeDots />
+            {/* <DropDownMenu isOpen={isDropDown}>
                         <ul onClick={deletePost} className={styles.deleteButton}>
                             <li>Delete post</li>
                         </ul>
-                    </DropDownMenu>
-                </div>
-                : null}
-        </div>
-        <div className={styles.textContainer}>
-            {postText}
-        </div>
-        {postImgURL ? <img className={styles.postImg} src={postImgURL} alt='post_image' /> : null}
-    </div>;
+                    </DropDownMenu> */}
+          </div>
+        ) : null}
+      </div>
+      <Menu
+        open={Boolean(anchorEl)}
+        anchorEl={anchorEl}
+        disableAutoFocus={true}
+        onClose={handleClickAway}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem onClick={deletePost}>Delete post</MenuItem>
+      </Menu>
+      <div className={styles.textContainer}>{postText}</div>
+      {postImgURL ? (
+        <img className={styles.postImg} src={postImgURL} alt="post_image" />
+      ) : null}
+    </div>
+  );
 }
