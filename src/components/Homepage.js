f646949088fc4ebba4d965d918f6dd9d77a2b49a
Raymond Zou <raymond.1zou@gmail.com>
2023-12-02 23:11:11 -0800

    add sign in method to button

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -11,7 +11,7 @@ export default function Homepage(props) {
         <>
             <div className='flex justify-center items-center flex-col bg-blue-100 h-screen gap-5'>
                 <h1 className="text-5xl">LinkedOut</h1>
-                <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Login with Google</button>
+                <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={props.signInMethod}>Login with Google</button>
             </div>           {/* <div>
                 <div >Linked<span>out</span></div>
                 <div >
