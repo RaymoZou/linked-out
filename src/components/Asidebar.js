b2e10bce86ce17eb77f8f6b401c08cb42a591e45
Raymond Zou <raymond.1zou@gmail.com>
2023-12-12 22:37:52 -0800

    remove asidebar module

--- src/components/Asidebar.js
+++
@@ -1,8 +0,0 @@
-// import styles from '../styles/Asidebar.module.css';
-
-// export default function Asidebar() {
-//     return (
-//         <div className={`${styles.scaffoldAside} ${styles.outline}`}>
-//         </div>
-//     )
-// }
