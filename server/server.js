b40f446c0f0f0eadca8e1d357f1aa022005862cd
Raymond Zou <raymond.1zou@gmail.com>
2024-01-14 23:27:36 -0800

    add express server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// TODO:
+app.get('/', (req, res) => {
+	res.json({ message: 'hello world' });
+})
+
+// TODO:
+// get a post by ID
+app.get('/post/:id', (req, res) => {
+	console.log(req.params.id)
+	res.json({ message: `this is the post with id ${req.params.id}` });
+});
+
+app.listen(port, () => {
+	console.log(`Listening on port: ${port}`)
+});
