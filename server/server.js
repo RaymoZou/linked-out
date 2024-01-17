c6fbcd26016e499f59fdcc8b41e98e69cfc4f85f
Raymond Zou <raymond.1zou@gmail.com>
2024-01-16 23:58:30 -0800

    add mongodb

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
-const express = require('express')
+const express = require('express');
+const mongodb = require('./database');
+require('dotenv').config()
+
 const app = express()
 const port = 3000
 
