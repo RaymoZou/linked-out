84416fec438c3871d0c15d4912882a39ee7d8464
Raymond Zou <raymond.1zou@gmail.com>
2024-01-27 17:37:03 -0800

    add back css import statement

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// this is required for tailwindcss to work for nested children
+import './index.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
   <App/>
 );
