7b35b0f7d1d7f2d342cfb87b185003926088ee12
raymond <raymond.1zou@gmail.com>
2024-02-09 12:20:52 -0800

    add Delete button

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,17 +1,20 @@
 export default function Post(props) {
+    // get post_id passed as prop so it can be deleted via http DELETE request
     const { name, text } = props;
 
     // TODO: send API request to delete post
-    // function deletePost() {
-    //     console.log(`deleting post: ${postId}`);
-    // }
+    function deletePost() {
+        console.log(`deleting post:`);
+    }
 
     // TODO: conditional rendering of delete button
     return (
-        <div className="bg-blue-300 border p-4 mx-4 flex gap-4 rounded">
-            {/* <img src={photoURL} alt="" /> */}
-            <div className="bg-yellow-200 p-2 text-2xl rounded">{name}</div>
-            <div className="bg-green-500 p-2 rounded text-xl">{text}</div>
+        <div className="bg-blue-300 border p-4 mx-4 flex justify-between gap-4 rounded">
+            <div className="flex justify-between gap-4">
+                <div className="bg-yellow-200 p-2 text-2xl rounded">{name}</div>
+                <div className="bg-green-500 p-2 rounded text-xl">{text}</div>
+            </div>
+            <button onClick={deletePost} className="bg-red-500 rounded p-2 text-lg font-bold text-white hover:bg-red-600" >Delete</button>
         </div>
     );
 }
