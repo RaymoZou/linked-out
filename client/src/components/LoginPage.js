a379881bd34ef33b41087f1385821322d757a6ec
Raymond Zou <raymond.1zou@gmail.com>
2024-02-11 13:47:08 -0800

    Add login page

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,6 +1,12 @@
 import axios from "axios";
+import { useState } from "react";
 
 export default function LoginPage({ setUser }) {
+
+    // login screen by default
+    const [isLoggingIn, setIsLoggingIn] = useState(true);
+
+    // TODO: form sanitation (no empty fields, etc.)
     async function login(e) {
         e.preventDefault();
         const username = e.target.username.value
@@ -9,13 +15,27 @@ export default function LoginPage({ setUser }) {
         if (res.status === 200) setUser({ username });
     }
 
+    async function signup(e) {
+        e.preventDefault();
+        const username = e.target.username.value
+        const password = e.target.password.value
+        const res = await axios.post(`/signup`, { username, password }, { withCredentials: true });
+        if (res.status === 200) setUser({ username });
+    }
+
     return (
         <div className='flex justify-center items-center flex-col h-screen gap-5'>
-            <form onSubmit={login} className="flex flex-col gap-2">
+            <div> {isLoggingIn ? "Log in" : "Sign up"} </div>
+            <form onSubmit={isLoggingIn ? login : signup} className="flex flex-col gap-2">
                 <input id="username" type="text" placeholder="username" className="p-2 rounded outline-none" autoComplete="on" />
                 <input id="password" type="password" placeholder="password" className="p-2 rounded outline-none" autoComplete="on" />
-                <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' >Login</button>
+                <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' >{isLoggingIn ? "Login" : "Sign Up"}</button>
             </form>
+            <button onClick={() => setIsLoggingIn(!isLoggingIn)}>{isLoggingIn
+                ? "Don't have an account yet? Sign Up!"
+                :
+                "Already have an account? Sign In!"}
+            </button>
         </div>
     )
 }
