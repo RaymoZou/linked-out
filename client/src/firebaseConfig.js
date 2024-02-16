8e3ba8b279fe495a20d68c811f21bf05fdc3907a
raymond <raymond.1zou@gmail.com>
2024-02-16 09:50:48 -0800

    remove old firebase configs

--- client/src/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyC-NbO8dXul0fZCrWbbm--FPIJimcMpuLI",
-  authDomain: "linkedout-31478.firebaseapp.com",
-  projectId: "linkedout-31478",
-  storageBucket: "linkedout-31478.appspot.com",
-  messagingSenderId: "148185206353",
-  appId: "1:148185206353:web:d946bd67a572fd710df270"
-};
