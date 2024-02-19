69e935fe0dcd7c5761201f90e1fa53a637b1b387
Raymond Zou <raymond.1zou@gmail.com>
2024-02-18 23:43:36 -0800

    add user context to sign in

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,14 @@
+import { useContext } from "react";
+import { UserContext } from "../App";
+
 export default function Navbar(props) {
-	return (
-		<div className="flex justify-end bg-slate-400 p-5">
-			<button className="bg-blue-500 rounded p-2 text-lg font-bold text-white hover:bg-blue-600" onClick={props.signOut}>Logout</button>
-		</div>);
+
+    const user = useContext(UserContext);
+
+    return (
+        <div className="flex justify-end bg-slate-400 p-5">
+            <div> Welcome back, {user.username}! </div>
+            <button className="bg-blue-500 rounded p-2 text-lg font-bold text-white hover:bg-blue-600" onClick={props.signOut}>Logout</button>
+        </div>
+    );
 };
