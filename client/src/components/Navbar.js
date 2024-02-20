dc37cfae167dfd2af766025942418f343e3dcefa
Raymond Zou <raymond.1zou@gmail.com>
2024-02-20 14:01:13 -0800

    add styling to navbar welcome message

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,8 +6,8 @@ export default function Navbar(props) {
     const user = useContext(UserContext);
 
     return (
-        <div className="flex justify-end bg-slate-400 p-5">
-            <div> Welcome back, {user.username}! </div>
+        <div className="flex justify-end items-center gap-4 bg-slate-400 p-5">
+            <div className="font-bold text-lg text-white"> Welcome back, {user.username}! </div>
             <button className="bg-blue-500 rounded p-2 text-lg font-bold text-white hover:bg-blue-600" onClick={props.signOut}>Logout</button>
         </div>
     );
