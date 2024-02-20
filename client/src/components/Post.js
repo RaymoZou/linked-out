774d1bb40b7c2cf93932ffe1e9e006ae271a5e69
Raymond Zou <raymond.1zou@gmail.com>
2024-02-19 19:51:15 -0800

    add conditional rendering of delete button

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
+import { useContext } from 'react';
+import { UserContext } from "../App";
 
 export default function Post(props) {
     const { name, text, postId } = props;
+    const userContext = useContext(UserContext);
 
     async function deletePost() {
         try {
@@ -9,20 +12,16 @@ export default function Post(props) {
             const res = await axios.delete("/post", { data, withCredentials: true });
             console.log(res.status);
         } catch (error) {
-            // TODO: render an user not authorized error message
-            // low priority since delete button can just be conditionally rendered
             console.log(error.response.status);
         }
     }
 
-    // TODO: conditional rendering of delete button
     // TODO: responsiveness at multiple screen resolutions
-    // TODO: prevent stretching of other flex items
     return (
         <div className="flex flex-col items-start bg-lightgreen gap-2 p-4 rounded">
             <div className="bg-yellow">{name}</div>
             <div className="break-all">{text}</div>
-            <button onClick={deletePost} className="bg-red-500 hover:bg-red-600 rounded p-2 text-lg font-bold text-white self-end" >Delete</button>
+            {name === userContext.username ? <button onClick={deletePost} className="bg-red-500 hover:bg-red-600 rounded p-2 text-lg font-bold text-white self-end" >Delete</button> : null}
         </div>
     );
 }
