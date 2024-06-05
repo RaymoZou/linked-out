5a6652ac3e1cef4f725faa06719f460b034e7c44
Raymond <raymond.1zou@gmail.com>
2024-06-04 20:50:15 -0700

    remove button component

--- client/src/components/Button.jsx
+++
@@ -1,5 +0,0 @@
-// TODO: figure out why colors are being properly set
-// export default function Button({ color, onClick, children }) {
-//     const buttonClass = `transition bg-${color}-500 hover:bg-${color}-600 text-lg text-white font-bold p-2 rounded`;
-//     return <button className={buttonClass} onClick={onClick ? onClick : null}>{children}</button>;
-// };
